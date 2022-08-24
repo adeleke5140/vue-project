5b6e894f5e9a5e542508f6663de5e7d751ef0cca
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-08-24 15:12:09 +0100

    add in markup for cityView route

---
+++ src/views/CityView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Suspense>
+      <AsyncCityView />
+
+      <template #fallback>
+        <p
+          class="text-white font-Roboto flex items-center justify-center px-2 py-2 w-full"
+        >
+          Loading...
+        </p>
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script setup>
+import AsyncCityView from "../components/AsyncCityView.vue"
+</script>
